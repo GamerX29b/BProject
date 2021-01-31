e7e80e95c3b5f2103f3de9b2f268d3c295c09896
Sergei <sergej-93@mail.ru>
2021-01-31 18:06:00 +0600

    Запрос и получение данных с AProject

--- src/main/java/Bproject/Bprojectsystem/сontrollers/GeneralPage.java
+++ src/main/java/Bproject/Bprojectsystem/сontrollers/GeneralPage.java
@@ -2,6 +2,10 @@ package Bproject.Bprojectsystem.сontrollers;
 
 import Bproject.Bprojectsystem.brokerClass.BrokerReceiver;
 import Bproject.Bprojectsystem.ejbClasses.EjbCaller;
+import Bproject.Bprojectsystem.jaxbComponent.Client;
+import Bproject.Bprojectsystem.jaxbComponent.JaxbConverterImpl;
+import Bproject.Bprojectsystem.jaxbComponent.Order;
+import Bproject.Bprojectsystem.jaxbComponent.Product;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.naming.NamingException;
 import javax.servlet.http.HttpServlet;
+import java.util.LinkedList;
+import java.util.List;
 
 @Controller
 public class GeneralPage extends HttpServlet {
@@ -22,8 +28,12 @@ public class GeneralPage extends HttpServlet {
     BrokerReceiver brokerReceiver;
 
     @Autowired
-    public GeneralPage(BrokerReceiver brokerReceiver) {
+    JaxbConverterImpl jaxbConverter;
+
+    @Autowired
+    public GeneralPage(BrokerReceiver brokerReceiver, JaxbConverterImpl jaxbConverter) {
         this.brokerReceiver = brokerReceiver;
+        this.jaxbConverter = jaxbConverter;
     }
 
     @RequestMapping(value = { "/", "/hello" }, method = RequestMethod.GET)
--- src/main/java/Bproject/Bprojectsystem/сontrollers/dataFromOrders.java
+++ src/main/java/Bproject/Bprojectsystem/сontrollers/dataFromOrders.java
@@ -1,25 +1,42 @@
 package Bproject.Bprojectsystem.сontrollers;
 
 
+import Bproject.Bprojectsystem.brokerClass.BrokerReceiver;
+import Bproject.Bprojectsystem.jaxbComponent.Client;
 import Bproject.Bprojectsystem.jaxbComponent.Order;
 import Bproject.Bprojectsystem.jaxbComponent.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
+import javax.ws.rs.HttpMethod;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import java.io.DataOutputStream;
 import java.io.IOException;
 
 import java.io.StringWriter;
 import java.io.Writer;
 import java.math.BigInteger;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @Controller
@@ -27,47 +44,33 @@ public class dataFromOrders {
 
     Logger log = Logger.getLogger(dataFromOrders.class.getName());
 
+    @Autowired
+    BrokerReceiver brokerReceiver;
+
+    public dataFromOrders(BrokerReceiver brokerReceiver) {
+        this.brokerReceiver = brokerReceiver;
+    }
+
     /**
      * Получить продукты по айди ордера
-     * @param orderId
+     * @param
      * @return
      */
     @POST
-    @Path("/orders/product")
+    @PostMapping("/orders/product")
     public String getProductById(String productId) {
-        System.out.println(productId);
 
-
-//        BrokerReceiver brokerReceiver = new BrokerReceiver();
-//        int id = 0;
-//        try {
-//            id = Integer.valueOf(orderId.replaceAll("[^0-9]",""));
-//        } catch (IncompatibleClassChangeError e){
-//            log.log(Level.SEVERE, e.getMessage());
-//        }
-//        try {
-//            URL url = new URL("http://localhost:8082/Aproject-system-1.0/fromBase");
-//            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-//            urlConnection.setRequestMethod("GET");
-//        } catch (IOException e) {
-//            log.log(Level.SEVERE, e.getMessage());
-//        }
-//
-//        Product product = brokerReceiver.takeClient();
-//        List<Product> listProduct = order.getProduct();
-//        //TODO дописать когда будет готов модуль получения массива из брокера
-        //test
-        Product product = new Product();
-        product.setId(BigInteger.valueOf(1));
-        product.setQuantity(BigInteger.valueOf(4));
-        product.setNameProduct("Сосопыл");
-
-
-
-        List<Product> listProduct = new ArrayList();
-        listProduct.add(product);
-        //test
-
+        final RestTemplate restTemplate = new RestTemplate();
+        int id = 0;
+        try {
+            id = Integer.valueOf(productId.replaceAll("[^0-9]",""));
+            restTemplate.put("http://localhost:8082/Aproject-system-1.0/getProductDataFromClientId", id);
+        } catch (IncompatibleClassChangeError e) {
+            log.log(Level.SEVERE, e.getMessage());
+        }
+        Order order = brokerReceiver.receiveOrder();
+        List<Product> listProduct = order.getProduct();
+        //TODO дописать когда будет готов модуль получения массива из брокера
         StringBuilder readyJson = new StringBuilder();
         for (int i = 0 ; i < listProduct.size() ; i++){
             Product oneProduct = listProduct.get(i);
@@ -93,34 +96,40 @@ public class dataFromOrders {
      * @return
      */
 
-    @RequestMapping(value = { "/orders/orders" }, method = RequestMethod.POST)
+    @PostMapping(value = { "/orders/orders" })
     public String getOrdersById(@RequestParam(value = "orderId", defaultValue = "0")String orderId) {
-
-
-        Order order = new Order();
-        order.setId(BigInteger.valueOf(3));
-        order.setQuantity(BigInteger.valueOf(4));
-
-        List<Order> listOrders = new ArrayList<>();
-        listOrders.add(order);
-        //test
-
-        StringBuilder readyJson = new StringBuilder();
-        for (int i = 0 ; i < listOrders.size() ; i++){
-            Order orderList = listOrders.get(i);
-            JsonObjectBuilder objectBuilder = Json.createObjectBuilder()
-                    .add("id", orderList.getId().toString())
-                    .add("quantity", orderList.getQuantity().toString())
-                    .add("date", orderList.getDate().getDay()+"."+orderList.getDate().getMonth()+"."+orderList.getDate().getYear());
-            JsonObject jsonObject = objectBuilder.build();
-            try (Writer writer = new StringWriter()){
-                Json.createWriter(writer).write(jsonObject);
-                readyJson.append(writer.toString());
-                if (i < listOrders.size()-1) readyJson.append(",");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        final RestTemplate restTemplate = new RestTemplate();
+        MultiValueMap<String, String> headers = new HttpHeaders();
+        headers.add("id", orderId);
+        try {
+           URL url = new URL("http://localhost:8082/Aproject-system-1.0/getClientDataFromId?id="+orderId);
+           HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            log.log(Level.INFO, String.valueOf(urlConnection.getResponseCode()));
+        } catch (IncompatibleClassChangeError | MalformedURLException | ProtocolException e) {
+            log.log(Level.SEVERE, e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        Client client = new Client();
+            client = brokerReceiver.receiveClient();
+            List<Order> listOrders = client.getOrder();
+            StringBuilder readyJson = new StringBuilder();
+            for (int i = 0; i < listOrders.size(); i++) {
+                Order orderList = listOrders.get(i);
+                JsonObjectBuilder objectBuilder = Json.createObjectBuilder()
+                        .add("id", orderList.getId().toString())
+                        .add("quantity", orderList.getQuantity().toString())
+                        .add("date", orderList.getDate().getDay() + "." + orderList.getDate().getMonth() + "." + orderList.getDate().getYear());
+                JsonObject jsonObject = objectBuilder.build();
+                try (Writer writer = new StringWriter()) {
+                    Json.createWriter(writer).write(jsonObject);
+                    readyJson.append(writer.toString());
+                    if (i < listOrders.size() - 1) readyJson.append(",");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
         return "{ \"data\": [ " + readyJson.toString() + "]}";
     }
 }
